d310e62ddb5e1f6a5b31d9eea6f0ece684d767de
md. soliman <seo.mdsoliman@gmail.com>
2024-06-16 10:42:43 +0600

    I have added sementic tag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react"
 import './App.css';
 import Home from "./Home";
+import Logo from "../src/Assets/logo.jpeg"
 function App() {
   return (
-    <div >
+    <>
       <header >
-        <Home/>
+        <nav>
+          <img src={Logo} alt="logo"/>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Testimonials</a></li>
+            <li><a href="#">Order Online</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </nav>
        
       </header>
-    </div>
+      <footer>
+        <h6>All right reseve for Little Lemon Restaurant</h6>
+      </footer>
+    </>
   );
 }
 
